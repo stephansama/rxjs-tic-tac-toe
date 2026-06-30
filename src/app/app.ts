e2c5aac903236f5f4bc03481c43b2c40e06b6b8f
stephansama <stephanrandle@hotmail.com>
2026-06-29 23:04:05 -0400

    chore: connect cells to app state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type PlayerType = 'First' | 'Second';
+
+type Cell = {
+  index: number;
+  player?: PlayerType;
+};
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,6 +16,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('app');
-  protected readonly cells = signal(Array.from({ length: 9 }));
-  protected readonly playerOneMoves = signal([]);
+  protected readonly cells = signal(
+    Array.from(
+      { length: 9 },
+      (_, i) =>
+        ({
+          index: i,
+          player: undefined,
+        }) satisfies Cell,
+    ),
+  );
+  protected readonly currentPlayer = signal('First' as PlayerType);
+  protected click(index: Cell) {
+    console.info(index.index);
+  }
 }
